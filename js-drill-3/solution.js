743e607e7af09c6e7ca7d6e55aba62cdb7b159c4
Harrish Easwar <harisheaswar@gmail.com>
2024-09-06 14:23:06 +0530

    Added solution 5 and test 5 to js-drill-3

--- js-drill-3/solution.js
+++ js-drill-3/solution.js
@@ -81,3 +81,25 @@
         return yearArr;
     }
 
+
+// ==== Problem #5 ====
+  // The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
+  
+
+  export function findOlderCars(inputYear=2000){
+
+    const yearArray= findAllCarYears();
+    const olderCarsArr=[];
+
+    if(typeof inputYear!="number"){
+        return ("Invalid Input. Please enter an year(number)");
+    }
+
+    for(let index=0;index<yearArray.length;index++){
+        if(yearArray[index]<inputYear){
+            olderCarsArr.push(inventory[index]);
+        }
+    }
+
+    return olderCarsArr;
+  }
--- js-drill-3/test.js
+++ js-drill-3/test.js
@@ -1,4 +1,4 @@
-import {findCarById,findLastCar,sortByModel,findAllCarYears} from "./solution.js";
+import {findCarById,findLastCar,sortByModel,findAllCarYears,findOlderCars} from "./solution.js";
 
 
 
@@ -109,3 +109,29 @@ import {findCarById,findLastCar,sortByModel,findAllCarYears} from "./solution.js
     console.log("Test with params: ");  
     const findAllYearsTest2=findAllCarYears(2019);
     console.log(findAllYearsTest2); 
+
+
+// ==== Problem #5 ====
+    // The car lot manager needs to find out how many cars are older than the year 2000. Using the array you just obtained from the previous problem, find out how many cars were made before the year 2000 and return the array of older cars and log its length.
+
+
+    console.log("Testing for function findOlderCars(): ");
+
+    //With No Params
+    console.log("Test with no params: ");                                               //default is set to year=2000;
+    const findOlderCarsTest1=findOlderCars();
+    console.log(findOlderCarsTest1.length);
+
+    //With correct Params
+    console.log("Test with correct params: ");                                          //Input any custom year
+    const findOlderCarsTest2=findOlderCars(2000);
+    console.log(findOlderCarsTest2.length);
+
+
+    //With inappropriate params                                                         //Returns an error message when non numbers are passed as input
+    console.log("Test with inappropriate params: ");
+    const findOlderCarsTest3=findOlderCars("Hello World!!");
+    console.log(findOlderCarsTest3);
+
+
+
