abef064890665bffb3ee5dc99ea29c6efc460b6d
Harrish Easwar <harisheaswar@gmail.com>
2024-09-23 17:56:22 +0530

    refactor the code using fs-inbuilt promises

---
+++ promises-inbuilt/test/testProblem2.js
@@ -0,0 +1,15 @@
+import {
+  readFileAndSolve,
+  convertToUpperCaseAndWrite,
+  convertToLowerCaseAndWrite,
+  sortTextAndWrite,
+  deleteRecent,
+} from "../problem2.js";
+
+//test this function
+readFileAndSolve(
+  convertToUpperCaseAndWrite,
+  convertToLowerCaseAndWrite,
+  sortTextAndWrite,
+  deleteRecent
+);
